b977d1aac204e86e25da1f0368799c36e346c50f
priyansh-narang2308 <145008344+priyansh-narang2308@users.noreply.github.com>
2025-03-27 00:36:01 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import passport from "passport"
-import session from "express-session"
+import passport from "passport";
+import session from "express-session";
+import path from "path";
 
-import "./passport/githubAuth.js"
-import authRoutes from "./routes/authRoutes.js"
+import "./passport/githubAuth.js";
+import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import exploreRoutes from "./routes/exploreRoutes.js";
 
@@ -20,12 +21,20 @@ app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: fals
 app.use(passport.initialize());
 app.use(passport.session());
 
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 const PORT = process.env.PORT || 5000;
