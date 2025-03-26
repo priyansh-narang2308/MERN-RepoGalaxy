674f2a84b290afeebe944c4baf774e684ec73608
priyansh-narang2308 <145008344+priyansh-narang2308@users.noreply.github.com>
2025-03-27 00:27:48 +0530

    something

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     connectDb();
